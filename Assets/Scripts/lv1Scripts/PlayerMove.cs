f972f29a8b2857368fde14ed943f0d86c4a83514
JaimeCAST69 <20223tn088@utez.edu.mx>
2025-11-24 15:50:38 -0600

    [ADD] frutas y recollecion

--- Assets/Scripts/lv1Scripts/PlayerMove.cs
+++ Assets/Scripts/lv1Scripts/PlayerMove.cs
@@ -7,6 +7,13 @@ public class PlayerMove : MonoBehaviour
 
     Rigidbody2D rb2D;
 
+    public bool betterJump = false;
+
+    public float fallMultiplier = 0.5f;
+
+    public float lowJumpMultiplier = 1f;
+
+
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
@@ -30,6 +37,17 @@ public class PlayerMove : MonoBehaviour
         if((Input.GetKey("space") || Input.GetKey("up")) && CheckGround.isGrounded)
         {
             rb2D.linearVelocity = new Vector2(rb2D.linearVelocity.x, jumpSpeed);
+        }
+        if (betterJump)
+        {
+            if(rb2D.linearVelocity.y < 0)
+            {
+               rb2D.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
+            }
+            if(rb2D.linearVelocity.y > 0 && !Input.GetKey("space"))
+            {
+               rb2D.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+            }
         }
     }
 }
