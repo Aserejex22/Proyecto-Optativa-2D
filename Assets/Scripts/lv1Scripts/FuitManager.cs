72801e4d83ada6372896a4828f475de7c877e368
JaimeCAST69 <20223tn088@utez.edu.mx>
2025-12-05 10:08:56 -0600

    [ADD] score

--- Assets/Scripts/lv1Scripts/FuitManager.cs
+++ Assets/Scripts/lv1Scripts/FuitManager.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class FuitManager : MonoBehaviour
-{   
-   // public Text levelCleared;
-   
+{
+    //public Text levelCleared;
+
+    public Text totalFruits;
+    public Text fruitsCollected;
+
+    private int totalFruitsIntLevel;
+
+    void Start()
+    {
+        totalFruitsIntLevel = transform.childCount;
+    }
 
     public GameObject transition;
     private void Update()
     {
         AllFruitsCollected();
+        totalFruits.text = totalFruitsIntLevel.ToString();
+        fruitsCollected.text = (totalFruitsIntLevel - transform.childCount).ToString();
 
     }
     public void AllFruitsCollected()
@@ -17,7 +28,7 @@ public class FuitManager : MonoBehaviour
         if(transform.childCount == 0)
         {
             Debug.Log("Nivel Completado");
-           // levelCleared.gameObject.SetActive(true);
+           //levelCleared.gameObject.SetActive(true);
             transition.SetActive(true);
             Invoke("ChangeScene", 1);
         }
