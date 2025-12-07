e1c5f22b5a70397e969cc93ea14b0ad5102279ce
Aserejex22 <20223tn097@utez.edu.mx>
2025-12-06 22:54:39 -0600

    [ADD] Menu Pausa

---
+++ Assets/Scripts/PauseMenu/PauseMenuManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject pauseButton;
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseButton.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+    
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pauseButton.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+    }
+}    
