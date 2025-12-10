18092dda13fcc181ea5b19e449a60d929b67c503
BojitaNoir <20213tn094@utez.edu.mx>
2025-12-10 02:36:36 -0600

    Endcredits corregidos y corregido persistencia de audio

--- Assets/Scripts/AudioPersistence.cs
+++ Assets/Scripts/AudioPersistence.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioPersistence : MonoBehaviour
 {
+    private const string MenuSceneName = "MenuMain";
+    private const string CreditsSceneName = "EndCredits";
+
     private void Awake()
     {
         int count = FindObjectsOfType<AudioPersistence>().Length;
@@ -13,5 +17,24 @@ public class AudioPersistence : MonoBehaviour
         {
             DontDestroyOnLoad(gameObject);
         }
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        string sceneName = scene.name;
+
+        if (sceneName == MenuSceneName || sceneName == CreditsSceneName)
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+            
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
